1067730d7981e073dae466027eb5846271881918
Leif Messinger <leifmessinger@gmail.com>
2022-02-27 17:18:52 -0600

    Added mongo to main page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,18 +1,25 @@
+<svelte:head>
+	<title>GoVote Home</title>
+</svelte:head>
+
 <script context="module" lang="ts">
 	import Banner from '$lib/Banner.svelte';
 	import Card from '$lib/petition/card.svelte';
 	import { v4 } from '@lukeed/uuid';
-	const cards = await this.fetch(`petitions.json`).then(r => r.json()).then(posts => {
-		return { posts };	//Extract all petitions
-	});
+
+	export async function load({ fetch }) {
+		const res = await fetch('/petitions.json');
+		const petitions = await res.json();
+
+		return {
+			props: { petitions }
+		};
+	}
 </script>
 
-<script lang="ts">
+<script>
+	export let petitions;
 </script>
-
-<svelte:head>
-	<title>Home</title>
-</svelte:head>
 
 <div class="flex h-full w-full flex-grow flex-col">
 	<Banner>
@@ -22,11 +29,17 @@
 		</div>
 	</Banner>
 	<section class="container mx-auto my-6 px-2 md:px-0">
+		<!--{#await petitionsPromise}
+			GoLoad...
+		{:then petitions}-->
 		<h2 class="mb-4">Recent Petitions</h2>
 		<div class="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
-			{#each cards as petition}
+			{#each petitions as petition}
 				<Card {...petition} id={v4()} />
 			{/each}
 		</div>
+		<!--{:catch error}
+			GoFail. :(
+		{/await}-->
 	</section>
 </div>
--- src/routes/petitions/[id].svelte
+++ src/routes/petitions/[id].svelte
@@ -1,18 +1,19 @@
 <!-- This file generates a detailed petition page -->
 <script context="module">
+	//The formatting of the page is defined in Page.svelte 
+	import Page from './Page.svelte';
+
 	/** @type {import('@sveltejs/kit').Load} */
-	export const load = async ({ params, query }) => {
+	export const load = async ({ params, fetch}) => {
 		//I think since we named the thing [id] then the id gets stored in params.id
 		const petition = this.fetch("blog/" + params.id + ".json").then(r => r.json());
 		return {
-			petition: petition
+			props: petition
 		}
 	};
 </script>
 
 <script>
-	//The formatting of the page is defined in Page.svelte 
-	import Page from './Page.svelte';
 	export let petition;
 </script>
 
