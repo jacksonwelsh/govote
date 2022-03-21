1ad76b392758345c8c64c7610f02ecafabb71366
Jackson Welsh <me@jacksonwel.sh>
2022-03-20 19:49:46 -0500

    fix compile error

--- src/lib/layout/Footer.svelte
+++ src/lib/layout/Footer.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <footer
-	class= bg-slate-200 py-6 underline-offset-1 dark:bg-slate-800 text-center"
+	class="bg-slate-200 py-6 underline-offset-1 dark:bg-slate-800 text-center"
 >
 	Created by
 	<!-- Iterate over authors and join appropriately. -->
