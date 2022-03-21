296754ed1d3344da8719fa188b27bba1af7d21a6
Jackson Welsh <me@jacksonwel.sh>
2022-03-20 22:48:07 -0500

    update adapter, env loading

--- src/environment.ts
+++ src/environment.ts
@@ -1,19 +1,22 @@
 import { envsafe, str, url } from 'envsafe';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 /**
  * Declare all environment variables here and access through this interface.
  */
 const env = envsafe({
-	NODE_ENV: str({
-		choices: ['development', 'test', 'production'],
-		devDefault: 'development'
-	}),
-	DB_CONN_STRING: url({
-		devDefault: 'mongodb://localhost'
-	}),
-	DB_NAME: str({
-		default: 'govote'
-	})
+  NODE_ENV: str({
+    choices: ['development', 'test', 'production'],
+    devDefault: 'development',
+  }),
+  DB_CONN_STRING: url({
+    devDefault: 'mongodb://localhost',
+  }),
+  DB_NAME: str({
+    default: 'govote',
+  }),
 });
 
 export default env;
