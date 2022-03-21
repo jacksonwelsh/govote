b7340955def628347e623cb49af1a44b69123d92
Jackson Welsh <me@jacksonwel.sh>
2022-03-20 20:13:31 -0500

    rudimentary petition markdown rendering

--- src/lib/petition/Page.svelte
+++ src/lib/petition/Page.svelte
@@ -1,10 +1,20 @@
 <script>
+  import showdown from 'showdown';
+
   export let _id = '';
   export let title = _id || '';
   export let description = '';
+
+  const converter = new showdown.Converter();
+
+  const html = converter.makeHtml(description);
 </script>
 
-<div class="prose container mx-auto mt-4">
+<div
+  class="container prose prose-lg prose-invert mx-auto mt-4 prose-ol:list-decimal prose-ul:list-disc"
+>
   <h1 class="my-2">{title}</h1>
-  {description}
+  <div class="">
+    {@html html}
+  </div>
 </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,16 @@
 module.exports = {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
-	theme: {
-		fontFamily: {
-			sans: ['"Public Sans"', 'ui-sans-serif', 'system-ui', 'sans-serif'],
-			serif: ['Merriweather', 'ui-serif', 'Georgia', 'serif']
-		},
-		extend: {
-			backgroundImage: {
-				'flutter-pattern': "url('/flutter.svg')",
-				'flutter-light': "url('/flutter-light.svg')"
-			}
-		}
-	},
-	plugins: [require('@tailwindcss/forms')]
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    fontFamily: {
+      sans: ['"Public Sans"', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      serif: ['Merriweather', 'ui-serif', 'Georgia', 'serif'],
+    },
+    extend: {
+      backgroundImage: {
+        'flutter-pattern': "url('/flutter.svg')",
+        'flutter-light': "url('/flutter-light.svg')",
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 };
