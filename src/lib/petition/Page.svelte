173ad20158a8288c41f855303f1855cf0b55258f
Jackson Welsh <me@jacksonwel.sh>
2022-03-24 11:59:37 -0500

    temporarily support legacy db schema

--- src/lib/petition/Page.svelte
+++ src/lib/petition/Page.svelte
@@ -4,16 +4,17 @@
 
   export let _id = '';
   export let title = _id || '';
-  export let description = '';
+  export let desc = '';
+  export let description = desc || '';
 
   const converter = new showdown.Converter();
 
   const html = converter.makeHtml(description);
 </script>
 
-<article class="grid grid-cols-1 md:grid-cols-4 container mx-auto">
+<article class="container mx-auto grid grid-cols-1 md:grid-cols-4">
   <div
-    class="prose prose-lg dark:prose-invert col-span-3 mx-auto mt-4 prose-h1:text-4xl prose-ol:list-decimal prose-ul:list-disc"
+    class="prose prose-lg col-span-3 mx-auto mt-4 prose-h1:text-4xl prose-ol:list-decimal prose-ul:list-disc dark:prose-invert"
   >
     <h1 class="mt-2 mb-10 !text-6xl">{title}</h1>
     <div class="">
