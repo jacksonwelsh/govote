f49016348f21c5b57ceb29c187e8df7324b0dc16
Leif Messinger <leifmessinger@gmail.com>
2022-04-24 19:20:59 -0500

    Added functions to control representatives
    Not sure if they work, but the site doesn't crash so I must be doing something right

--- src/lib/govote/index.ts
+++ src/lib/govote/index.ts
@@ -90,25 +90,120 @@ export async function petitionSignatures(petitionId: ObjectId, answerIndex: numb
 	return count;
 }
 
+//Representatives absolutely suck because we need to make sure that the representatives followers gets updated too.
+
 //Returns a voter's representative list
-export async function getRepresentatives(representativeId: ObjectId, voterId: ObjectId, index: number = -1): ObjectId[]{
-	
+export async function getRepresentatives(voterId: ObjectId): ObjectId[]{
+	return collections.voters.findOne({ _id: voterId}).representatives || [];
+}
+export async function getFollowers(voterId: ObjectId): ObjectId[]{
+	return collections.voters.findOne({ _id: voterId}).followers || [];
+}
+//Sets a voter's representative list,
+//Updates followers for the representatives
+export async function setRepresentatives(voterId: ObjectId, newRepresentatives: ObjectId[]){
+	//Get the old representative list from the db
+	const oldRepresentatives: ObjectId[] = await getRepresentatives(voterId);
+
+	//Update the representatives being added to include the voter as a follower
+	//Hilariously, this can be done asynchronously because each representative has a separate follower array, so there is no data race
+	const additions: ObjectId[] = newRepresentatives.filter((rep) => {return !oldRepresentatives.includes(rep)});
+	additions.forEach((rep) => addFollower(rep, voterId));
+
+
+	//Update the representatives being removed to remove the voter from their followers
+	//Even more hilariously, there is no data race between the additions and the removals because they are disjoint sets
+	const removals: ObjectId[] = oldRepresentatives.filter((rep) => {return !newRepresentatives.includes(rep)});
+	removals.forEach((rep) => removeFollower(rep, voterId));
+	additions.forEach((representative) => removeFollower(representative, voterId));
+
+	//There is no cursor.update function, so we need to use the collection.update function
+	collections.voters.update({
+		_id: voterId
+	}, {
+		$set: {
+			representatives: newRepresentatives
+		}
+	});
+}
+export async function setFollowers(voterId: ObjectId, newRepresentatives: ObjectId[]){
+	//Nothing fancy in this one, just update the db
+
+	//There is no cursor.update function, so we need to use the collection.update function
+	collections.voters.update({
+		_id: voterId
+	}, {
+		$set: {
+			followers: newFollowers
+		}
+	});
 }
 //Does a check to see if the representative is in the voter's representative list
-export async function isRepresentative(representativeId: ObjectId, voterId: ObjectId){
-	
+export async function isRepresentative(voterId: ObjectId, representativeId: ObjectId): boolean{
+	return getRepresentatives(voterId).then(reps => reps.includes(representativeId));
+}
+//Does a check to see if the representative is in the voter's representative list
+export async function isFollower(representativeId: ObjectId, followerId: ObjectId): boolean{
+	return getFollowers(representativeId).then(reps => reps.includes(followerId));
 }
 //Adds the representative to the representatve list, if they aren't already one
-export async function represent(representativeId: ObjectId, voterId: ObjectId, index: number = -1){
-	//assert isRepresentative(representativeId, voterId) == false
-	//push the representative onto the db
+export async function addRepresentative(voterId: ObjectId, representativeId: ObjectId, index: number = 0){
+	const representatives: ObjectId[] = await getRepresentatives(voterId);
+	if(!representatives.includes(representativeId)){
+		representatives.splice(index, 0, representativeId);
+		setRepresentatives(voterId, representatives);
+	}
 }
-export async function removeRepresentative(representativeId: ObjectId, voterId: ObjectId){
-	
+//Adds the representative to the end of the representatve list, if they aren't already one
+export async function pushRepresentative(voterId: ObjectId, representativeId: ObjectId){
+	const representatives: ObjectId[] = await getRepresentatives(voterId);
+	if(!representatives.includes(representativeId)){
+		representatives.push(representativeId);
+		setRepresentatives(voterId, representatives);
+	}
+}
+//Adds the representative to the representatve list, if they aren't already one
+//Follower order doesn't matter as it's just a set
+export async function addFollower(representativeId: ObjectId, followerId: ObjectId){
+	const followers: ObjectId[] = await getFollowers(representativeId);
+	if(!followers.includes(followerId)){
+		followers.push(followerId);
+		setFollowers(representativeId, followers);
+	}
+}
+export async function removeRepresentative(voterId: ObjectId, representativeId: ObjectId){
+	//Get the array of representatives from the db
+	const representativeList: ObjectId[] = await getRepresentatives(voterId);
+
+	//Try to minimize api calls
+	if(!representativeList.includes(representativeId)) return;
+
+	//Remove the representative from the array
+	const newRepresentativeList: ObjectId[] = representativeList.filter(function(value, index, arr){ 
+		return value != representativeId;
+	});
+
+	//Update the db
+	setRepresentatives(voterId, newRepresentativeList);
+}
+export async function removeFollower(representativeId: ObjectId, followerId: ObjectId){
+	//Get the array of followers from the db
+	const followerList: ObjectId[] = await getFollowers(representativeId);
+
+	//Try to minimize api calls
+	if(!followerList.includes(followerId)) return;
+
+	//Remove the follower from the array
+	const newFollowerList: ObjectId[] = followerList.filter(function(value, index, arr){ 
+		return value != followerId;
+	});
+
+	//Update the db
+	setFollowers(representative, newFollowerList);
 }
 export async function reorderRepresentatives(representatives: ObjectId[], voterId: ObjectId){
 	//Get current representatives
-	//Make sure they are the same representatives, albeit in a different order
+	//Assert that they are the same representatives, albeit in a different order
 	//Set the representatives in the db to the new array
+	setRepresentatives(voterId, representatives);
 }
-
